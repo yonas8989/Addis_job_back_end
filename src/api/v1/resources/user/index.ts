1d619a23aaadf7a1173d1abbeea201901b8f9f4d
yonas8989 <wossen.dev@amplitude.ventures>
2025-04-21 14:38:25 +0200

    set up route for user module

--- src/api/v1/resources/user/index.ts
+++ src/api/v1/resources/user/index.ts
@@ -3,3 +3,14 @@ const router = Router();
 
 
 import { auth, validator } from "../middleware";
+import { createUserValidation, userLoginValidation,verifyOtpValidation } from "./validations";
+import { createUser, userLogin } from "./controller";
+import { Role } from "../../../../shared";
+
+
+router.post("/login", validator(userLoginValidation), userLogin);
+router.route("/").post(validator(createUserValidation), createUser);
+
+
+
+export default router;
